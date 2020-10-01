345d1d3676437ebeacd82c205bada2ce4a1cb67b
Jonatan Sombié <jonatan.sombie@pactilis.com>
2020-10-01 18:49:15 +0200

    wrap liquibase

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const liquibase = require("liquibase");
-const path = require('path')
+const path = require("path");
+const { exec } = require("child_process");
 
 const changeLogFile = core.getInput("changelog-file");
 const url = core.getInput("db-url");
@@ -9,15 +10,40 @@ const username = core.getInput("username");
 const password = core.getInput("password");
 const action = core.getInput("action");
 
-const config = {
-  liquibase: path.join(__dirname, 'liquibase', 'liquibase'),
-  changeLogFile,
-  url: `"${url}"`,
-  username: `"${username}"`,
-  password: `"${password}"`,
-};
+function run(action, url, changeLogFile) {
+  return new Promise((resolve, reject) => {
+    exec(
+      `${path.join(
+        __dirname,
+        "liquibase",
+        "liquibase"
+      )} --classpath="${path.join(
+        __dirname,
+        "drivers",
+        "mssql-jdbc-jre14.jar"
+      )}" --changeLogFile="${changeLogFile}" --url="${url}" --logLevel=info ${action}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        console.error(stderr);
+        console.log(stdout);
+        resolve()
+      }
+    );
+  });
+}
 
-liquibase(config)
-  .run(action)
+// const config = {
+//   liquibase: path.join(__dirname, "liquibase", "liquibase"),
+//   changeLogFile,
+//   url: `"${url}"`,
+//   username: `"${username}"`,
+//   password: `"${password}"`,
+// };
+
+// liquibase(config)
+run(action, url, changeLogFile)
   .then((result) => console.log(result))
-  .catch(err => core.setFailed(err));
+  .catch((err) => core.setFailed(err));
